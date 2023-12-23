39f378080f8f031a1eaab36a96f94ffaad6f21d9
Martin Wechsler <martin.wechsler@student.tugraz.at>
2023-12-23 09:01:38 +0100

    21,22

--- 21/main.py
+++ 21/main.py
@@ -1,9 +1,160 @@
 import time
+import numpy as np
+from heapq import heappop, heappush, heapify
+
+
+def print_map(lines, poss, starty, endy, startx, endx, S, p=False):
+    if not p:
+        return
+    for y in range(starty, endy):
+        if y >= len(lines) or y < 0:
+            continue
+        for x in range(startx, endx):
+            if x >= len(lines[y]) or x < 0:
+                continue
+
+            pos = (y, x)
+            if S == pos:
+                print('S', end='')
+                continue
+            if pos in poss:
+                print('O', end='')
+            else:
+                print(lines[y][x], end='')
+        print()
+
+
+def check_corner(lines, pos):
+    # left
+    pos_y = pos[0]
+    pos_x = pos[1] - 1
+    left_block = False
+    if pos_x < 0 or pos_x >= len(lines[0]) or lines[pos_y][pos_x] == '#':
+        left_block = True
+    # right
+    pos_y = pos[0]
+    pos_x = pos[1] + 1
+    right_block = False
+    if pos_x < 0 or pos_x >= len(lines[0]) or lines[pos_y][pos_x] == '#':
+        right_block = True
+    # top
+    pos_y = pos[0] - 1
+    pos_x = pos[1]
+    top_block = False
+    if pos_y < 0 or pos_y >= len(lines) or lines[pos_y][pos_x] == '#':
+        top_block = True
+    # bottom
+    pos_y = pos[0] + 1
+    pos_x = pos[1]
+    bottom_block = False
+    if pos_y < 0 or pos_y >= len(lines) or lines[pos_y][pos_x] == '#':
+        bottom_block = True
+
+    return left_block and right_block and top_block and bottom_block
+
+
+def get_distances(lines, S, debug=False):
+    d = [[np.inf for _ in range(len(lines[0]))] for _ in range(len(lines))]
+    y = len(lines)
+    x = len(lines[0])
+    S_y, S_x = S
+
+    d[S_y][S_x] = 0
+    aft = [(np.sqrt((S_y - y) ** 2 + (S_x - x) ** 2), y, x) for y in range(len(lines)) for x in range(len(lines[0])) if
+           lines[y][x] != '#']
+    aft.remove((0, S_y, S_x))
+    heapify(aft)
+
+    while len(aft) > 0:
+        aha, pos_y, pos_x = heappop(aft)  # aft.pop(0)
+        up, bottom, left, right = np.inf, np.inf, np.inf, np.inf
+        if 0 <= (pos_y + 1) < len(lines):
+            bottom = d[pos_y + 1][pos_x] + 1
+        if 0 <= (pos_y - 1) < len(lines):
+            up = d[pos_y - 1][pos_x] + 1
+        if 0 <= (pos_x + 1) < len(lines[0]):
+            right = d[pos_y][pos_x + 1] + 1
+        if 0 <= (pos_x - 1) < len(lines[0]):
+            left = d[pos_y][pos_x - 1] + 1
+        dist = min(d[pos_y][pos_x], up, bottom, left, right)
+        if dist == np.inf and not check_corner(lines, (pos_y, pos_x)):
+            heappush(aft, (aha + 1, pos_y, pos_x))  # aft.append((aha, pos_y, pos_x))
+        d[pos_y][pos_x] = dist
+
+    if debug:
+        [print(di) for di in d]
+        for i in range(0, y):
+            for j in range(0, x):
+                if lines[i][j] != '.':
+                    print(lines[i][j], end='')
+                    continue
+
+                print("x" if d[i][j] == np.inf else d[i][j] % 10, end='')
+            print()
+
+    return d
+
+
+def write_out(lines, d, steps):
+    file = open("output.txt", 'w')
+    new_lines = []
+
+    for y in range(len(lines)):
+        s = []
+        for x in range(len(lines[0])):
+            if lines[y][x] == '#':
+                s.append('#')
+                continue
+
+            if d[y][x] < steps:
+                s.append(str(d[y][x] % 10))
+            else:
+                s.append(lines[y][x])
+
+        new_lines.append(''.join(s))
+
+    file.write('\n'.join(new_lines))
 
 
 def task():
     input_file = open('input.txt' if not testing else 'example.txt', 'r')
     lines = input_file.readlines()
+    lines = [line.split()[0] for line in lines]
+    index = ''.join(lines).find('S')
+    S_y = index // len(lines[0])
+    S_x = index - (len(lines[0]) * S_y)
+    S = (S_y, S_x)
+
+    steps = 64
+    debug = False
+    distances = get_distances(lines, S, debug)
+
+    step_count = 0
+    count_even, count_odd = 0, 0
+    count_ev_corner, count_od_corner = 0, 0
+    for line in distances:
+        for distance in line:
+            if distance % 2 == 0 and distance < np.inf:
+                count_even += 1
+            if distance % 2 == 1 and distance < np.inf:
+                count_odd += 1
+            if distance % 2 == 1 and steps + 1 < distance < np.inf:
+                count_od_corner += 1
+            if distance % 2 == 0 and steps + 1 < distance < np.inf:
+                count_ev_corner += 1
+            if distance % 2 == 0 and distance <= steps:
+                step_count += 1
+
+    if stage == 1:
+        write_out(lines, distances, steps)
+        print("Stage 1:", step_count)
+        return
+
+    stage2_steps = 26501365
+    n = stage2_steps // 131
+    print(n)
+    calc = (n + 1) ** 2 * count_odd + (n ** 2) * count_even - (n + 1) * count_od_corner + n * count_ev_corner
+    print("Stage 2:", calc)
 
 
 if __name__ == '__main__':
--- 22/main.py
+++ 22/main.py
@@ -1,9 +1,134 @@
 import time
+from functools import cmp_to_key
+import numpy as np
+
+
+def get_blocks(start, end):
+    blocks = []
+    for x in range(start[0], end[0] + 1):
+        for y in range(start[1], end[1] + 1):
+            for z in range(start[2], end[2] + 1):
+                blocks.append([x, y, z])
+
+    return np.array(blocks)
+
+
+def compare(item1, item2):
+    z1 = item1[1][0][2]
+    z2 = item2[1][0][2]
+
+    if z1 < z2:
+        return -1
+
+    if z1 > z2:
+        return 1
+
+    return 0
+
+
+def rm(current, dis, above, under):
+    if under[current].issubset(dis):
+        dis.add(current)
+    else:
+        return 0
+
+    counter = 0
+    for brick_above in above[current]:
+        counter += rm(brick_above, dis, above, under)
+
+    return counter + 1
+
+
+def write_to_file(grid):
+    file = open("output.txt", 'w')
+    out_lines = []
+    max_x, max_y, max_z = grid.shape
+    for x in range(max_x):
+        for y in range(max_y):
+            out_lines.append(' '.join([str(int(c)).zfill(4) for c in grid[x, y, :]]))
+
+    file.write('\n'.join(out_lines))
+
+
+def build_grid(bricks, max_x, max_y, max_z):
+    grid = np.zeros((max_x + 1, max_y + 1, max_z + 1))  # 3 dimensional grid to set positions of bricks
+    lowest_z = np.zeros((max_x + 1, max_y + 1))  # smallest z where block could fall down to
+    above = {brick[0]: set() for brick in bricks}  # blocks above brick_id(key)
+    under = {brick[0]: set() for brick in bricks}  # blocks under brick_id(key)
+
+    for i, brick in enumerate(bricks):
+        brick_id, blocks = brick
+        smallest_diff = max_z
+        # get highest z where brick can fall to
+        for block in blocks:
+            pos_x, pos_y, pos_z = block
+            diff = pos_z - lowest_z[pos_x, pos_y]
+            smallest_diff = min(diff - 1, smallest_diff)
+
+        blocks[:, 2] = blocks[:, 2] - smallest_diff
+        bricks_under = set()
+        # adjusting lowest_z array and building above/under sets
+        for block in blocks:
+            pos_x, pos_y, pos_z = block
+            if (block_under := grid[pos_x, pos_y, pos_z - 1]) != 0:
+                bricks_under.add(block_under)
+                above[block_under].add(brick_id)
+
+            lowest_z[pos_x, pos_y] = pos_z
+        under[brick_id] = bricks_under
+        #bricks[i] = (brick_id, blocks)
+        for block in blocks:
+            grid[tuple(block)] = brick_id
+
+    return grid, under, above
 
 
 def task():
     input_file = open('input.txt' if not testing else 'example.txt', 'r')
     lines = input_file.readlines()
+
+    # (id, [blocks])
+    bricks = []  # all bricks
+    max_x, max_y, max_z = 0, 0, 0
+    ids = 1
+    # print("READ LINES")
+    for line in lines:
+        coords = line.strip().split('~')
+        start = [int(n) for n in coords[0].split(',')]
+        end = [int(n) for n in coords[1].split(',')]
+
+        blocks = get_blocks(start, end)
+        max_x = max(np.max(blocks[:, 0]), max_x)
+        max_y = max(np.max(blocks[:, 1]), max_y)
+        max_z = max(np.max(blocks[:, 2]), max_z)
+
+        brick = (ids, blocks)
+        bricks.append(brick)
+        ids += 1
+
+    bricks = sorted(bricks, key=cmp_to_key(compare))
+    grid, under, above = build_grid(bricks, max_x, max_y, max_z)
+    print(under)
+    print(above)
+
+    fall = {}
+    for brick in bricks:
+        brick_id = brick[0]
+        supporting = above[brick_id]
+
+        fall_set = {brick_id}
+        counter = 0
+        for brick_above in supporting:
+            counter += rm(brick_above, fall_set, above, under)
+
+        fall[brick_id] = counter
+
+    if stage == 1:
+        print("Stage 1:", list(fall.values()).count(0))
+        return
+
+    print("Stage 2:", sum(list(fall.values())))
+    write_to_file(grid)
 
 
 if __name__ == '__main__':
