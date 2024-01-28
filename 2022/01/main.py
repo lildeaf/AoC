fec156ddf3ddbf057db2ad00958fc2a4014be26f
Martin Wechsler <martin.wechsler@student.tugraz.at>
2024-01-28 13:50:26 +0100

    2022.1

---
+++ 2022/01/main.py
@@ -0,0 +1,34 @@
+import time
+
+
+def task():
+    input_file = open('input.txt' if not testing else 'example.txt', 'r')
+    lines = input_file.readlines()
+
+    elves = []
+    sums = 0
+    for line in lines:
+        num = line.strip()
+        if len(num) == 0:
+            elves.append(sums)
+            sums = 0
+            continue
+        sums += int(num)
+    else:
+        elves.append(sums)
+
+    elves.sort(reverse=True)
+    top = 3 if stage == 2 else 1
+    print(sum(elves[0:top]))
+
+
+if __name__ == '__main__':
+    testing = False
+    stage = 1
+    start = time.time()
+    task()
+    print(time.time() - start)
+    stage = 2
+    start = time.time()
+    task()
+    print(time.time() - start)
